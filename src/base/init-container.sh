17f6d1a2c1372f7641322f6de0b5f570d6319b88
Trevor Lauder <trevor.l@getjobber.com>
2026-03-31 12:28:27 -0600

    Update init-container.sh (#19)
    * Update init-container.sh
    
    Signed-off-by: Trevor Lauder <trevor.l@getjobber.com>
    
    * Execute post-init script if available
    
    Add post-init script execution after installation.
    
    Signed-off-by: Trevor Lauder <trevor.l@getjobber.com>
    
    * Update version to 1.1.9 in devcontainer feature
    
    Signed-off-by: Trevor Lauder <trevor.l@getjobber.com>
    
    ---------
    
    Signed-off-by: Trevor Lauder <trevor.l@getjobber.com>

--- src/base/init-container.sh
+++ src/base/init-container.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
+post_init_cmd=/usr/local/bin/post-init.sh
+
 rm -Rf ~/.gnupg
 
 mise trust
 eval "$(mise activate bash)"
 
 mise install --yes
+
+
+if command -v ${post_init_cmd}; then
+  ${post_init_cmd}
+fi
 
 sudo /usr/local/sbin/init-firewall.sh
