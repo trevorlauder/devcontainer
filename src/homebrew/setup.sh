d4b0a8be565c15af7e035b6261bd7b17dd955809
Trevor Lauder <me@trevorlauder.dev>
2026-04-14 20:54:37 -0600

    add postStartCommand for homebrew (#69)

--- src/homebrew/setup.sh
+++ src/homebrew/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir -p "${HOMEBREW_PREFIX}/Homebrew" "${HOMEBREW_PREFIX}/bin"
-
 curl -fsSL "https://github.com/Homebrew/brew/archive/refs/tags/${BREW_VERSION}.tar.gz" | \
   tar -xz --strip-components=1 -C "${HOMEBREW_PREFIX}/Homebrew"
 
